26abaea26a882e0470e4970df1e1d632f18add26
jade tang <jadetangcn@qq.com>
2014-11-18 23:17:34 +0800

    Initial commit

---
+++ src/main/java/com/dudu/datastructure/Trie.java
@@ -0,0 +1,26 @@
+package com.dudu.datastructure;
+
+import com.sun.xml.internal.org.jvnet.fastinfoset.RestrictedAlphabet;
+
+import java.nio.charset.Charset;
+import java.util.*;
+
+/**
+ * Created by Administrator on 14-11-18.
+ */
+public class Trie<V> extends AbstractMap<String,V> implements Map<String,V> {
+    HashMap map;
+
+    public Trie(Alphabet alpabet ){
+
+     }
+
+    @Override
+    public Set<Entry<String, V>> entrySet() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
